29dfac9400f234596f716e70890a6b06d80db213
aaron-patton <98922929+aaron-patton@users.noreply.github.com>
2023-02-19 15:04:41 -0500

    Add files via upload

---
+++ knowledgeCheck1.py
@@ -0,0 +1,22 @@
+print("hello world!")
+
+simpleList = ["List Item 1", "List Item 2", "List Item 3"]
+
+print('The second list item = ') 
+print(simpleList[1])
+
+Dict = {1:'Value 1', 2:'Value 2'}
+
+print ('Dictionary Key 1 = ') 
+print(Dict[1])
+
+Tuple1 = (1, 'two', 3, 'four')
+
+print('Fourth Tuple Value = ')
+print(Tuple1[3])
+
+
+
+
+
+
